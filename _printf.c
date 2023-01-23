31ee725986137dcfca0f8b0459150e3cbbd642ae
Suleiman Tijjani Shehu <suleimanshehutijjani@gmail.com>
2023-01-23 13:32:14 -0800

    Trying to debug the error issue

--- _printf.c
+++ _printf.c
@@ -54,7 +54,6 @@ return (printed_chars);
 * @buffer: Array of chars
 * @buff_ind: Index at which to add next char, represents the length.
 */
-
 void print_buffer(char buffer[], int *buff)
 {
 if (*buff > 0)
